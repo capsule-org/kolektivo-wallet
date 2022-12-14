efb8df90e678b4ad3390a5af0e4a6f87b1ad7f44
osdnk <micosa97@gmail.com>
2022-12-14 15:17:38 +0200

    Signer module

---
+++ ios/CapsuleSignerModule.m
@@ -0,0 +1,14 @@
+//
+//  CapsuleSignerModule.m
+//
+//  Created by Michał Osadnik on 14/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import "CapsuleSignerModule.h"
+
+@implementation CapsuleSignerModule
+
+RCT_EXPORT_MODULE();
+
+@end
