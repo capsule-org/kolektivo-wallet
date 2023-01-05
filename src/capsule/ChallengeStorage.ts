7fe70a4ed746566ec9be1bc3db73a194cbd20c87
osdnk <micosa97@gmail.com>
2023-01-05 17:23:02 +0200

    Real signer

--- src/capsule/ChallengeStorage.ts
+++ src/capsule/ChallengeStorage.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import elliptic, { ec } from 'elliptic'
 // @ts-ignore
 import EllipticSignature from 'elliptic/lib/elliptic/ec/signature'
@@ -28,8 +29,46 @@ export interface Signature {
 
 const ecl = new elliptic.ec('p256')
 
-const privateKey = '202d73cbde65f547c75613ace311393ac97f2556cbe3aca32bf48eb84ec2198c'
+const PEM_HEADER = '-----BEGIN PUBLIC KEY-----'
+const PEM_FOOTER = '-----END PUBLIC KEY-----'
 export class ChallengeReactNativeStorage extends ChallengeStorage {
+  private storageIdentifier() {
+    return 'challenge-' + this.userId
+  }
+  async getPublicKey(): Promise<string> {
+    const pemPublicKey = await DeviceCrypto.getOrCreateAsymmetricKey(this.storageIdentifier(), {
+      accessLevel: AccessLevel.ALWAYS,
+      invalidateOnNewBiometry: false,
+    })
+
+    const base64PublicKey = pemPublicKey.replace(PEM_FOOTER, '').replace(PEM_HEADER, '').trim()
+    const bufferPublicKey = Buffer.from(base64PublicKey, 'base64')
+    const publicKeyHexAsnPreamble = bufferPublicKey.toString('hex')
+    const publicKeyHex = publicKeyHexAsnPreamble.slice(52)
+    return publicKeyHex
+  }
+
+  async signChallenge(message: string): Promise<Signature> {
+    const signatureDERBase64 = await DeviceCrypto.sign(this.storageIdentifier(), message, {
+      biometryTitle: 'Authenticate',
+      biometrySubTitle: 'Signing',
+      biometryDescription: 'Authenticate your self to sign the text',
+    })
+
+    const signatureDERBuffer = Buffer.from(signatureDERBase64, 'base64')
+    const signatureDERHex = signatureDERBuffer.toString('hex')
+    const signature = new EllipticSignature(signatureDERHex, 'hex') as ec.Signature // hack due to incorrect typings
+    const cannonicalSignature = {
+      r: signature.r.toString('hex'),
+      s: signature.s.toString('hex'),
+      recoveryParam: signature.recoveryParam as number,
+    }
+    return cannonicalSignature
+  }
+}
+
+export class ChallengeFakeReactNativeStorage extends ChallengeStorage {
+  private static privateKey = '202d73cbde65f547c75613ace311393ac97f2556cbe3aca32bf48eb84ec2198c'
   async getPublicKey(): Promise<string> {
     return '0483326f8677519eace4e8db81722399ac4b581a91236656359ebf3621ad3186fdf2e1fa04c9929d577c36ffb9e2ef6cfe325d1da7ffa4d0a596bf88d7e335baf2'
   }
@@ -39,7 +78,9 @@ export class ChallengeReactNativeStorage extends ChallengeStorage {
     hash.update(message, 'utf8')
     const hashedMessage = hash.digest('hex')
 
-    const signature = ecl.keyFromPrivate(privateKey).sign(hashedMessage)
+    const signature = ecl
+      .keyFromPrivate(ChallengeFakeReactNativeStorage.privateKey)
+      .sign(hashedMessage)
     return {
       r: signature.r.toString('hex'),
       s: signature.s.toString('hex'),
@@ -47,60 +88,60 @@ export class ChallengeReactNativeStorage extends ChallengeStorage {
     }
   }
 }
+
 const test2 = async () => {
   const { userId } = await userManagementClient.createUser({
     email: `test-${uuidv4()}@test.usecapsule.com`,
   })
   Logger.debug('userId', userId)
   await userManagementClient.verifyEmail(userId, { verificationCode: '123456' })
-  const pk2 = await DeviceCrypto.getOrCreateAsymmetricKey('userid', {
+  const pemPublicKey = await DeviceCrypto.getOrCreateAsymmetricKey('userid', {
     accessLevel: AccessLevel.ALWAYS,
     invalidateOnNewBiometry: false,
   })
-  // extracting the base64 from PEM (TODO)
-  const base64Pk = pk2
-    .split('-')
-    .find((e) => e.length > 50)!
-    .trim()
-  const buffer = Buffer.from(base64Pk, 'base64')
-  const bufString = buffer.toString('hex')
-  const hexpk = bufString.slice(52)
-  await userManagementClient.addBiometrics(userId, { publicKey: hexpk })
+
+  const base64PublicKey = pemPublicKey.replace(PEM_FOOTER, '').replace(PEM_HEADER, '').trim()
+  const bufferPublicKey = Buffer.from(base64PublicKey, 'base64')
+  const publicKeyHexAsnPreamble = bufferPublicKey.toString('hex')
+  const publicKeyHex = publicKeyHexAsnPreamble.slice(52)
+  await userManagementClient.addBiometrics(userId, { publicKey: publicKeyHex })
   const challenge = await userManagementClient.getBiometricsChallenge(userId)
   const message = challenge.data.challenge
 
-  const publicKey = ecl.keyFromPublic(hexpk, 'hex')
-  const hash = crypto.createHash('sha256')
-  hash.update(message)
-  const hashedMessage = hash.digest('hex')
-  const signature = await DeviceCrypto.sign('userid', message, {
+  const publicKey = ecl.keyFromPublic(publicKeyHex, 'hex')
+  const messageHash = crypto.createHash('sha256')
+  messageHash.update(message)
+  const hashedMessage = messageHash.digest('hex')
+  const signatureDERBase64 = await DeviceCrypto.sign('userid', message, {
     biometryTitle: 'Authenticate',
     biometrySubTitle: 'Signing',
     biometryDescription: 'Authenticate your self to sign the text',
   })
 
-  const buffer2 = Buffer.from(signature, 'base64')
-  const sigHex = buffer2.toString('hex')
-  const sigParsed = new EllipticSignature(sigHex, 'hex') as ec.Signature // hack due to incorrect typings
-  const aigRR = {
-    r: sigParsed.r.toString('hex'),
-    s: sigParsed.s.toString('hex'),
-    recoveryParam: sigParsed.recoveryParam as number,
+  const signatureDERBuffer = Buffer.from(signatureDERBase64, 'base64')
+  const signatureDERHex = signatureDERBuffer.toString('hex')
+  const signature = new EllipticSignature(signatureDERHex, 'hex') as ec.Signature // hack due to incorrect typings
+  const cannonicalSignature = {
+    r: signature.r.toString('hex'),
+    s: signature.s.toString('hex'),
+    recoveryParam: signature.recoveryParam as number,
   }
 
-  const res = await userManagementClient.verifyBiometricsChallenge(userId, { signature: aigRR })
+  const res = await userManagementClient.verifyBiometricsChallenge(userId, {
+    signature: cannonicalSignature,
+  })
   console.log({ res })
 
-  const RES = publicKey.verify(hashedMessage, aigRR)
+  const RES = publicKey.verify(hashedMessage, cannonicalSignature)
 
   console.log(
     JSON.stringify(
       {
-        publicKey: hexpk,
-        publicKeyLen: hexpk.length,
+        publicKey: publicKeyHex,
+        publicKeyLen: publicKeyHex.length,
         hash: hashedMessage,
         result: RES,
-        signature: aigRR,
+        signature: cannonicalSignature,
       },
       null,
       2
@@ -108,4 +149,4 @@ const test2 = async () => {
   )
 }
 
-void test2()
+// void test2()
