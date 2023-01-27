055e6d5076f9db11f5f059fab52a933010722e74
osdnk <micosa97@gmail.com>
2023-01-28 00:05:23 +0200

    Extract out iOS files

--- capsule/ios/CapsuleSignerModule.m
+++ capsule/ios/CapsuleSignerModule.m
@@ -141,5 +141,13 @@ RCT_EXPORT_METHOD(refresh:(NSString*)protocolId
                          withObject:params];
 }
 
+// Don't compile this code when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeReactNativeWalletSpecJSI>(params);
+}
+#endif
 
 @end
