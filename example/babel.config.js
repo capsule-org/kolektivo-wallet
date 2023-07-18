ef36e9125a4aa570b2d641a4fc5e5e7c9ecc1472
osdnk <micosa97@gmail.com>
2023-07-18 21:12:13 +0300

    fixes :3

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,20 @@
+const path = require('path');
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          '@usecapsule/react-native-wallet': path.join(
+            __dirname,
+            '..',
+            'capsule',
+            'src',
+          ),
+        },
+      },
+    ],
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+const pack = require('../capsule/package.json');
+const nodeLibs = require('node-libs-react-native');
 
 /**
  * Metro configuration
@@ -6,6 +9,27 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+
+const modules = Object.keys(pack.peerDependencies);
+
+const root1 = path.resolve(__dirname, '..', 'capsule');
+const root2 = path.resolve(__dirname, '..', 'example');
+
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [root1, root2],
+  resolver: {
+    extraNodeModules: modules.reduce(
+      (acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      },
+      {
+        ...nodeLibs,
+        crypto: path.join(__dirname, 'node_modules', 'react-native-crypto'),
+      },
+    ),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
